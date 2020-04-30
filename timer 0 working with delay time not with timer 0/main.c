2bbbe9a8a58076905041e44150558660d0db1686
MagedAbdelaziz2020 <63506566+MagedAbdelaziz2020@users.noreply.github.com>
2020-05-01 01:34:04 +0200

    Add files via upload

---
+++ timer 0 working with delay time not with timer 0/main.c
@@ -0,0 +1,60 @@
+/*
+ * timer 0.c
+ *
+ * Created: 18/04/2020 05:57:33 م
+ * Author : MAGED
+ */ 
+#define F_CPU  16000000
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "bit_math.h"
+#include <util/delay.h>
+#define TOGGLE(var,bit)      var^=(1<<bit)
+volatile unsigned int counter=0;
+int main(void)
+{
+	volatile unsigned int counter=0;
+	//prescaler clk/64   CS02=0  CS01=1  CS00=1
+	TCCR0=(1<<CS00)|(1<<CS01); // 0000 0011
+	//Generate timer 0 Overflow interrupt enable
+	TIMSK=(1<<TOIE0);
+	//LED 0 output pin pc2
+	DDRC=(1<<2);     // led 0   pin pc2
+	DDRD=(1<<3);     // led 2   pin pd3
+	DDRC|=(1<<7);    // led 1   pin pc7
+	//Global interrupt
+	sei();
+	while(1)
+		{
+		if(counter<=1000)//100ms
+		{
+			    PORTC^=(1<<2);
+				_delay_ms(100);
+		        counter=0;
+	    	}
+			 if(counter<=2000)//100ms
+			{
+				PORTC^=(1<<7);
+				_delay_ms(150);
+				counter=0;
+			}
+			 if(counter<=3000)//100ms
+			{
+				PORTD^=(1<<3);
+				_delay_ms(200);
+				counter=0;
+			}
+
+     }
+}
+//main
+//*****************************
+ISR(TIMER0_OVF_vect){
+	counter++;
+}
+
+
+//PORTC^=(1<<2);   //toggle led 0
+//PORTC^=(1<<7);   //toggle led 1
+//PORTD^=(1<<3);   //toggle led 2
+
