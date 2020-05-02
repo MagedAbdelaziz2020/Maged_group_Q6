6891bbf91a7ec81f5db7df3ded1e541b09f356ce
MagedAbdelaziz2020 <63506566+MagedAbdelaziz2020@users.noreply.github.com>
2020-05-02 18:09:25 +0200

    Add files via upload

--- timer 0/main.c
+++ timer 0/main.c
@@ -13,35 +13,35 @@
 volatile unsigned int counter=0;
 int main(void)
 {
-	volatile unsigned int counter=0;
+	//volatile unsigned int counter=0;
 	//prescaler clk/64   CS02=0  CS01=1  CS00=1
 	TCCR0=(1<<CS00)|(1<<CS01); // 0000 0011
 	//Generate timer 0 Overflow interrupt enable
 	TIMSK=(1<<TOIE0);
 	//LED 0 output pin pc2
 	DDRC=(1<<2);     // led 0   pin pc2
-	DDRD=(1<<3);     // led 2   pin pd3
 	DDRC|=(1<<7);    // led 1   pin pc7
+	DDRD=(1<<3);     // led 2   pin pd3
 	//Global interrupt
 	sei();
 	while(1)
 		{
-		if(counter<=100)//100ms
+		if(counter>=50)//100ms
 		{
 			    PORTC^=(1<<2);
 				_delay_ms(10);
-		        counter=0;
+		      // counter=0;
 	    	}
-			 if(counter<=200)//100ms
+			 if(counter>=100)//100ms
 			{
 				PORTC^=(1<<7);
 				_delay_ms(10);
-				counter=0;
+				//counter=0;
 			}
-			 if(counter<=300)//100ms
+			 if(counter>=150)//100ms
 			{
 				PORTD^=(1<<3);
-				_delay_ms(100);
+				_delay_ms(10);
 				counter=0;
 			}
 
